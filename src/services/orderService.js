06d41bc87c7738b6bf124a2277d288a1e5e4acf2
keyl1yy <keyly.la@fordeer.io>
2023-06-16 13:24:26 +0700

    update status order

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -4,3 +4,6 @@ import { CREATE_ORDER_API } from './apiRoutes'
 export const createOrder = (data) => {
     return axios.post(CREATE_ORDER_API, data, requestFormData())
 }
+export const updateStatusOrder = (data, id) => {
+    return axios.patch(`${CREATE_ORDER_API}/${id}`, data)
+}
